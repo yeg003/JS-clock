a5da9de5700c7d0c4c7aee761a64ad562a5fd265
yeg003 <yeg003@gmail.com>
2019-03-31 12:46:10 -0400

    added 12 hour time

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 function getTime() {
     let fullDate = new Date();
-    let hour = fullDate.getHours()
-    let min = fullDate.getMinutes()
-    let sec = fullDate.getSeconds()
+    let hours = fullDate.getHours()
+    let mins = fullDate.getMinutes()
+    let secs = fullDate.getSeconds()
 
-    document.getElementById('hours').innerHTML = hour + ":"
-    document.getElementById('minutes').innerHTML = min + ":"
-    document.getElementById('seconds').innerHTML = sec
+    if (hours < 10) {
+        hours = '0' + hours
+    } if (mins < 10) {
+        mins = '0' + mins
+    } if (secs < 10) {
+        secs = '0' + secs
+    }
+
+    document.getElementById('hours').innerHTML = hours + ":"
+    document.getElementById('minutes').innerHTML = mins + ":"
+    document.getElementById('seconds').innerHTML = secs
+
+
 }
 
 setInterval(getTime, 1000)
